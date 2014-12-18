319bfe089c0152898c53f2d42b8866f7e21d3a73
btbvoy <p.meled.in@gmail.com>
2014-12-18 12:09:56 +0200

    added TimeoutableFuture with a 'within' method

--- src/test/scala/com/wix/async/TimeoutableFutureTest.scala
+++ src/test/scala/com/wix/async/TimeoutableFutureTest.scala
@@ -1,7 +1,8 @@
 package com.wix.async
 
-import java.util.concurrent.Executors
+import java.util.concurrent.{TimeUnit, ScheduledThreadPoolExecutor, CountDownLatch, Executors}
 
+import com.twitter.util.TimerTask
 import com.wix.async.FuturePerfect.Successful
 import com.wix.async.helpers.FooOperation
 import org.specs2.mutable.{SpecificationWithJUnit, Specification}
@@ -24,17 +25,62 @@ class TimeoutableFutureTest extends SpecificationWithJUnit with NoTimeConversion
     def theExecutionOf[T] = waitFor[T] _
 
     def waitFor[T](future: Future[T]): T = {
-      Await.result(future)
+      Await.result(future, 1.second)
     }
-    val timeout = 100 millis
+    val timeout = 100.millis
+
+    implicit class TimeoutableFuture[T](f: Future[T]) {
+
+      def within(timeout: Duration) = {
+        val p = Promise[T]()
+
+        val terminator = Terminator(timeout) {
+          p.tryFailure(new TimeoutException(s"Future.within timed out after ${timeout.toMillis} millis"))
+        }
+
+        f.onComplete { result =>
+          p.tryComplete(result)
+          terminator.cancel
+        }
+
+        p.future
+      }
+
+      class Terminator(timeout: Duration, f: => Unit) {
+        import Terminator._
+        val runnable = new Runnable { def run() = f }
+        val javaf = scheduler.schedule(runnable, timeout.toMillis, TimeUnit.MILLISECONDS)
+
+        def cancel = {
+          javaf.cancel(true)
+          scheduler.remove(runnable)
+        }
+      }
+
+      object Terminator {
+        val scheduler = new ScheduledThreadPoolExecutor(1)
+        scheduler.setMaximumPoolSize(5)
+
+        def apply(timeout: Duration)(f: => Unit) = new Terminator(timeout, f)
+      }
+    }
   }
 
   "within" should {
     "comeback on time" in new Context {
       val f = Future { 1 }
-      val timeoutableF = TimeoutableFuture.within(f, new Timer, timeout)
+      theExecutionOf(f.within(timeout)) must beEqualTo(1)
+    }
 
-      theExecutionOf(timeoutableF) must throwA[TimeoutGaveUpException]
+    "fail if timeout has passed" in new Context {
+      val latch = new CountDownLatch(1)
+      val f = Future {
+        latch.await()
+      }
+
+      theExecutionOf(f.within(timeout)) must throwA[TimeoutException]("Future.within timed out after 100 millis")
+
+      latch.countDown()
     }
   }
 }
