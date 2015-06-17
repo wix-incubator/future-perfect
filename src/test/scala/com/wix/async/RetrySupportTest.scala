ccec10c6b22ec08adf242543209659916f47febb
Shai Nagar <shain@wix.com>
2015-06-17 09:39:32 +0300

    bug fix: RetrySupport ignores the delay strategy provided with RetryPolicy

--- src/test/scala/com/wix/async/RetrySupportTest.scala
+++ src/test/scala/com/wix/async/RetrySupportTest.scala
@@ -1,23 +1,33 @@
 package com.wix.async
 
-import org.specs2.mutable.SpecificationWithJUnit
-import org.specs2.mock.Mockito
-import org.specs2.specification.Scope
 import org.mockito.Mockito._
+import org.specs2.mock.Mockito
+import org.specs2.mutable.SpecificationWithJUnit
+import org.specs2.specification.Scope
+import org.specs2.time.NoTimeConversions
+
+import scala.compat.Platform
+import scala.concurrent.duration._
 
 
 /**
  * User: avitaln
  * Date: 9/30/13
  */
-class RetrySupportTest extends SpecificationWithJUnit with Mockito {
+class RetrySupportTest extends SpecificationWithJUnit with Mockito with NoTimeConversions {
 
   trait Dummy {
     def func() : Int
+    def elapsedFromSpecStart(): Duration
   }
 
   trait Context extends Scope with RetrySupport {
     val dummy = mock[Dummy]
+    val startTime = Platform.currentTime
+    val elapsedTimeAnswer = new MockAnswer[Duration]({ _ => elapsedUntilNow })
+    val retryDelay: Duration = 200.millis
+
+    def elapsedUntilNow = (Platform.currentTime - startTime).millis
   }
 
   "withRetry" should {
@@ -31,6 +41,27 @@ class RetrySupportTest extends SpecificationWithJUnit with Mockito {
       result must_== 88
 
       there were two(dummy).func()
+    }
+
+    "delay retries according to the specified delay strategy" in new Context {
+      when(dummy.elapsedFromSpecStart())
+        .thenThrow(new IllegalArgumentException)
+        .thenAnswer(elapsedTimeAnswer)
+
+      val retryPolicyWithDelay = onceFor[IllegalArgumentException].copy(
+        delayStrategy = DelayStrategy.constant(retryDelay)
+      )
+
+      val elapsedUntilSecondRetry = withRetry(retryPolicyWithDelay) {
+        dummy.elapsedFromSpecStart()
+      }
+
+      elapsedUntilSecondRetry must {
+        beGreaterThanOrEqualTo(retryDelay) and
+        beLessThanOrEqualTo(elapsedUntilNow)
+      }
+
+      there were two(dummy).elapsedFromSpecStart()
     }
 
     "not retry on another exception" in new Context {
